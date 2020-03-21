d39c680ec4a9a72c1d64b618700287ca8a44998a
Piotr Ciazynski <pciazynski+foss@pm.me>
2020-03-21 19:10:54 +0100

    Move ball on space press

--- frozen.js
+++ frozen.js
@@ -1,7 +1,22 @@
+const dx = 1;
+const dy = 1;
+const radius = 25;
+
+let started = false;
+
+const circle = new Circle(50, 55, radius);
+const circle2 = new Circle(50, 595, radius);
+
 function Circle(x, y, r) {
   this.x = x;
   this.y = y;
   this.r = r;
+}
+
+function doKeyDown(e) {
+  if (e.keyCode == 32) {
+    started = true;
+  }
 }
 
 function drawBall(ctx, c) {
@@ -11,20 +26,24 @@ function drawBall(ctx, c) {
   ctx.fill();
 }
 
-function draw(ctx) {
-  const circle = new Circle(50, 55, 25);
+function draw(canvas) {
+  const ctx = canvas.getContext('2d');
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall(ctx, circle);
-
-  const circle2 = new Circle(50, 595, 25);
   drawBall(ctx, circle2);
+
+  if (started) {
+    if (circle2.y - 2 * radius - circle.y > 0) {
+      circle2.y += -dy;
+    }
+  }
 }
 
 function init() {
+  window.addEventListener("keydown", doKeyDown, false);
   const canvas = document.getElementById('bubbles');
   if (canvas.getContext) {
-    const ctx = canvas.getContext('2d');
-
-    const timer = setInterval(draw, 10, ctx);
+    const timer = setInterval(draw, 10, canvas);
     return timer;
   }
 }
